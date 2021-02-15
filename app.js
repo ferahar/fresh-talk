507bee32168c41b6a7a073356fb55bde1b2d0313
ferahar <ferahar@gmail.com>
2021-02-15 21:30:26 +0500

    edit for heroku

--- app.js
+++ app.js
@@ -5,37 +5,11 @@ const app = express()
 const port = process.env.PORT || 4200
 
 function startServer(port) {
-  app.use(
-      helmet.contentSecurityPolicy({
-        directives: {
-          defaultSrc: [
-            '\'self\'',
-            'https://ya-praktikum.tech',
-            'wss://ya-praktikum.tech',
-            'fonts.googleapis.com',
-            '\'nonce-eef8264c4994bf6409c518251ac7c9614446\''
-          ],
-          fontSrc: ['\'self\'', 'fonts.gstatic.com'],
-          styleSrc: ['\'self\'', 'fonts.googleapis.com', '\'unsafe-inline\''],
-          scriptSrc: ['\'self\'', '\'unsafe-eval\''],
-          objectSrc: ['\'none\''],
-          upgradeInsecureRequests: [],
-        },
-      })
-  );
-
-  app.use(helmet.hidePoweredBy())
-  app.use(helmet.xssFilter())
-
   app.use(express.static(path.join(__dirname, 'dist')))
-
   app.get('*', function(request, response) {
     response.sendFile(__dirname + '/dist/index.html')
   });
-
-  app.listen(port, () => {
-    console.log(`app start at http://localhost:${port}`)
-  })
+  app.listen(port, () => console.log(`Listener on ${port}`))
 }
 
 startServer(port)
